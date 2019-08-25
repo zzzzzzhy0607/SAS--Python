b11e917748279d6a73c965e5204916e9eb79041b
zzzzzzhy0607 <43594014+zzzzzzhy0607@users.noreply.github.com>
2019-08-25 15:37:38 +0800

    Update Bookings_Tracker_Excel.py

--- Closure Report/Previous Booking Tracker Code/Bookings_Tracker_Excel.py
+++ Closure Report/Previous Booking Tracker Code/Bookings_Tracker_Excel.py
@@ -3,9 +3,9 @@ import time
 import pyodbc
 import pandas as pd
 
-LOC_QUERY = r'\\goaspen\Department_Shares\PricingRevMan\Analytics\02 - Infrastructure\Bookings Tracker\SQL\Bookings_Tracker.txt'
-LOC_OUTPUT = r'\\goaspen\Department_Shares\PricingRevMan\Analytics\02 - Infrastructure\Bookings Tracker\Data\Bookings_Tracker_Data.csv'
-LOC_HISTORICAL = r'\\goaspen\Department_Shares\PricingRevMan\Analytics\02 - Infrastructure\Bookings Tracker\Data\Historical\Bookings_Tracker_Data_'+datetime.datetime.now().strftime('%Y%m%d%H%M')+'.csv'
+LOC_QUERY = r'\\xxxx\SQL\Bookings_Tracker.txt'
+LOC_OUTPUT = r'\\xxxx\Data\Bookings_Tracker_Data.csv'
+LOC_HISTORICAL = r'\\xxxx\Data\Historical\Bookings_Tracker_Data_'+datetime.datetime.now().strftime('%Y%m%d%H%M')+'.csv'
 SERVER_NAME = 'xxx.xxx.xx.xxx,xxxxx'
 DATABASE_NAME = 'xxxxxxxxx'
 
@@ -28,5 +28,5 @@ def execute_sql_query(server_name, database_name,query_path):
 
 bookings_tracker_df = execute_sql_query(SERVER_NAME, DATABASE_NAME, LOC_QUERY)
 
-with pd.ExcelWriter(r'\\goaspen\Department_Shares\PricingRevMan\Analytics\02 - Infrastructure\Bookings Tracker\Data\Bookings_Tracker_Data.xlsx') as writer:
+with pd.ExcelWriter(r'\\xxxx\Data\Bookings_Tracker_Data.xlsx') as writer:
     bookings_tracker_df.to_excel(writer,index=False)
